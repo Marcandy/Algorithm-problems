54e651419cc63443ad4ceaea29125ad73652f16e
marcandy <marcandy.nj@gmail.com>
2017-01-31 23:58:13 -0700

    function

---
+++ reachme-summydigit.js
@@ -0,0 +1,20 @@
+
+
+
+
+// first map through the last element keep incrementing by initial value
+
+function sumDigNthTerm(initval, patternl, nthterm) {
+    // your code
+    for(var i = 1, j; i <= initval; i++) {
+
+      initval += patternl[j];
+      if(j < patternl.length -1) {
+        j++
+      }else {
+        j = 0
+      }
+    }
+
+    initval.toSting().spl
+}
