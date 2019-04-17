b0eb5550a097478cb61512559f29b6fd3046ee48
marcandy <mnoeljeu@jcp.com>
2019-04-17 15:07:00 -0500

    improving javascript custom code challenge

---
+++ javascript-daily/fundamentals/Overhall-interview-tips.md
@@ -0,0 +1,22 @@
+	• Start with a small personal story of ur own -- then go through your experience
+	• Be authenticate and show '
+
+	• Things not to say
+		○ There are different ways of approching architecture
+
+	• Send an email if you were not able to answer a technical question as a follow up
+		○ Boby languange - confidence and humility
+
+	• Math and science speak of universal truth do not get distracted by  lack of diversity
+		○ If indeed you did well in your interviw and you supect diversity played a part in not getting the position - then that is a place you would not want to work
+		○ Ask question - 'how do you support diversity' - you are also interviewing the interviewer
+	• When answering context - emphasis on outcome - do not talk to much thus making is hard for the interviewer to find conclusive outcomes
+	• Have the courage to bring counter argument when pushing your ideas -- with confidence
+		○ Be open to beeing wrong - and about questions you do not understant and want to seek knowledge 
+	• Technical panel or group interviews are like presentation
+	
+	
+	• Understanding the role avoid having to ramble in difficult question
+
+	• Avoid cliches -- interviews are like a conversation - tell your story
+When you have time to ask question -- you can use it to clarify stuff regarding you presentation - recover from difficult questions2
--- javascript-daily/fundamentals/TopList.js
+++ javascript-daily/fundamentals/TopList.js
@@ -2,6 +2,12 @@ function LinkedListNode(value) {
     this.value = value;
     this.next = null;
 }
+// creating a LinkedList invole setting up the value and node
+let linkedList = new LinkedListNode(9);
+console.log(linkedList.next);
+linkedList.next = 10;
+console.log(linkedList.next);
+
 
 function reverse(head) {
     let node = head,
@@ -24,8 +30,63 @@ function reverse(head) {
     }
 }
 
-// API design gudilnies ---https://hackernoon.com/restful-api-designing-guidelines-the-best-practices-60e1d954e7c9
+function reverseList(linkedList) {
+    // first I will save the node in current variable
+    // then I will I will have a previous and a nextVal variable 
+    
+    
+    let node = linkedList.head,
+        previous = null;
+     
+    
+    // we goin to loop over until we come to the very end of the linkedList
+    while(node) {
+      let save = node.next;
+      
+      // not breack it 
+      node.next = previous;// empty
+      
+      previous = node;
+      
+      //increment node to he next node 
+      node = save
+    }
+    
+    linkedList.head = previous;
+    return previous;
+  }
+  
+  var inputList = new LinkedList([1,2,3,4])
+  reverseList(inputList)
+  
+  console.log(inputList);
 
+
+  var reverseLinkedList = function(linkedlist) {
+    var node = linkedlist;
+    var previous = null;
+  
+    while(node) {
+      // save next or you lose it!!!
+      var save = node.next;
+      // reverse pointer
+      node.next = previous;
+      // increment previous to current node
+      previous = node;
+      // increment node to next node or null at end of list
+      node = save;
+    }
+    return previous;   // Change the list head !!!
+  }
+  linkedlist = reverseLinkedList(linkedlist);
+
+//===========================
+// API design gudilnies ---https://hackernoon.com/restful-api-designing-guidelines-the-best-practices-60e1d954e7c9
+/*
+http://api.yourservice.com/v1/companies/34/employees is a good example, which has the version number of the API in the path.
+If there is any major breaking update, we can name the new set of APIs as v2 or v1.x.x
+
+*/
 
 
 
@@ -33,33 +94,21 @@ function reverse(head) {
 // Life CYCLES --- REACT 
 
 	• componentWillMount, ComponentWillReceiveProps and componentWillUpdate will be depracated because of features like async rendering
-
-
-	• They will be rename to 
-		○ UNSAFE_componentWillMount
-		○ UNSAFE_componentDidMount
-		○ UNSAFE_componentWillUpdate
-	
-	
 	• Though two new lifecycle will be made 
 		○ getDerivedStateFromProps
 		○ getSnapShotBeforeUpdate
 
 
-
 -async rendering with componentWillmount will cause multiple rendering of the component tree
-
-	- You can use 'supense' for making api to server side the same way you use to for componentWillMount
-
+- You can use 'supense' for making api to server side the same way you use to for componentWillMount
 
 getDerivedStateFromProps
-	- This method is going to handle what componentWillRecieveProps was able to do along with componentDidUpdate. It is static. It is called after a component is created and also called when it receives a new prop. This will be the safer alternative to componentWillRecieveProps.
+    - This method is going to handle what componentWillRecieveProps was able to do along with componentDidUpdate.
+    It is static. It is called after a component is created and also called when it receives a new prop. This will be the safer alternative to componentWillRecieveProps.
 
-
-
--
 getSnapshotBeforeUpdate(prevProps, prevState) 
-This is going to handle what componentWillUpdate was able to do along with componentDidUpdate. This is called right before the DOM is updated. The value that is returned from getSnapshotBeforeUpdate is passed on to componentDidUpdate.
+This is going to handle what componentWillUpdate was able to do along with componentDidUpdate.
+This is called right before the DOM is updated. The value that is returned from getSnapshotBeforeUpdate is passed on to componentDidUpdate.
 
 /*
 	• Initial Render 
@@ -74,19 +123,28 @@ This is going to handle what componentWillUpdate was able to do along with co
 				□ getSnapshotbefore update -- replacing componentwillupdate
 			§ ComponentdidUpdate --- can do the same thing as componentdid mount
 		○ componenwillunmount
-			
-			
-	• 
-	• ComponentWIllMoun
-	• ComponentDiMount
-	• ComponenentWIll Receiveprop
-	• ComponentDidUpdate
-ComponentWillUnmount
-*/
+	Mounting
+These methods are called in the following order when an instance of a component is being created and inserted into the DOM:
+* constructor()
+* static getDerivedStateFromProps()
+* render()
+* componentDidMount()
+
+
+Updating
+An update can be caused by changes to props or state. These methods are called in the following order when a component is being re-rendered:
+
+* static getDerivedStateFromProps()
+* shouldComponentUpdate()
+* render()
+* getSnapshotBeforeUpdate()
+* componentDidUpdate()
+https://blog.bitsrc.io/understanding-react-v16-4-new-component-lifecycle-methods-fa7b224efd7d
 
 // --------------------------------------------------------------------
 // ---------------------------------------------------------------------
-// "Pick any topic you want: a hobby you have, a book you’ve read, a project you worked on–anything. You have five minutes to explain it." --- SSR -- Wordpress --- A man's search for meaning
+// "Pick any topic you want: a hobby you have, a book you’ve read, a project you worked on–anything.
+// You have five minutes to explain it." --- SSR -- Wordpress --- A man's search for meaning
 
 
 
@@ -122,8 +180,89 @@ Instances may be composed from many different source objects, allowing for easy 
 
 Javascript ---> event loop 
 Every time you call a setTimeout function or you do some async operation — it is added to the Event Table. This is a data structure which knows that a certain function should be triggered after a certain event. Once that event occurs (timeout, click, mouse move) it sends a notice. 
+IT send a notice for a nrew event to be added to the queue
+
+callStack --- eventQueue -- it checks contantly if ther is the callstack is empty then add the uppermost event function to the callstack
 */
 
+// Promises
+const myPromise = new Promise((resolve, reject) => {
+  resolve(1); // resolve the promise
+  reject('oop sorry'); // reject the promise with error
+});
+
+myPromise.then((x)=>{ // use .then for resolving promises
+  console.log(x);
+})
+
+myPromise.catch((err)=> {
+  console.log(err);
+})
+// ====================================
+const sum = (...args) => {
+  console.log(args);
+  let result = 0;
+  result = args.reduce((a , b) => {
+    return a + b;
+  })
+  return result;
+}
+
+// build  a tree for the following data:
+
+/*
+let data = [
+  {
+    title: "menu 1",
+    children :[
+      { title: "menu 1.1"},
+      { 
+        title: "menu 1.2",
+        children: [
+          {title: "menu 1.2.1"},
+          {title: "menu 1.2.2"},
+        ]
+      },
+    ]
+  },
+  {
+    title: "menu 2",
+    children :[
+      { title: "menu 2.1"},
+      { title: "menu 2.2"},
+    ]
+  } 
+]
+*/
+
+// Accepts two arguments
+// data-> the data to transform
+// isChild -> default false, used to indicate whether the node
+//            being rendered is a child element or not
+
+function buildTree(data, isChild = false) {
+  let html = '<ul>';
+
+  // run the forLoop over the data
+  data.forEach((d) => {
+    // for every data element render <li>
+    html += `<li>${d.title}</li>`
+    // if the current data element has children then call the
+    // buildTree again passing in the children and isChild = true
+    if (d.children) { // if it has children
+      html += buildTree(d.children, true);
+    }
+  });
+  // build the closing <ul> tag
+
+  // build the closing ul tag
+  html += '</ul>';
+  return html;
+}
+
+
+// ================================
+. Tree shaking or dead code elimination means that unused modules will not be included in the bundle during the build process.
 
 // ===============================================================
 1) You'll do better in front-end focused interviews if you're more productive at quickly implementing front-end features. You already know how to do this, but practicing it more will help you get faster and better at it
@@ -139,13 +278,123 @@ The Algorithm Design Manual: the third chapter : --https://teachyourselfcs.com/#
 
 
 //============================================React
-- Tell me your experience with React - What do you want to use to pass data from a parent component to a child component? - Have you used Redux? Tell me about what you've done with it - Could you give me some examples of why I would want to use React? Or to not use React? - Can you explain the life cycle methods? - Which data structures are immutable? Which ones are mutable? - Can you explain apply, call, and bind? - 
+/* - Tell me your experience with React - What do you want to use to pass data from a parent component to a child component? - 
+Have you used Redux? 
+Tell me about what you've done with it - Could you give me some examples of why 
+I would want to use React? Or to not use React? - Can you explain the life cycle methods? - Which data structures are immutable?
+ Which ones are mutable? - Can you explain apply, call, and bind? - 
+*/
 
 
 
-Event loop
 
 https://www.youtube.com/watch?v=8aGhZQkoFbQ&t=2s
 
 
 https://community.developer.visa.com/t5/Blogs/bg-p/Developer_Blog
+
+
+
+/*
+Active Javacript
+
+
+
+*/
+
+// remvove duplicate in 
+let arr = [3, 2, 1, 4, 6, 5, 7, 5]
+function removeDup(arr) {
+// we will have a unique holder
+// the loop through
+// have a hash table 
+// if if already there then dont put i
+let unique = [];
+let trakObj = {};
+for(let i = 0; i < arr.length; i++) {
+if(!trakObj.hasOnwProperty(arr[i])) {
+unique.push(arr[i]);
+trackObj[arri] = 1
+}
+}
+return unique
+}
+
+// reverse every word in a string
+let = 'fdkalsfj test bam';
+function reverseS(str) {
+// we will have a unique holder
+// the loop through
+// split the string in an array
+// loop backward 
+// add in a new array 
+// then join
+let tempStr = str.split('')
+let newHold = [];
+for(let i = tempStr.length - 1; i >= 0; i--) {
+newHold.push(tempStr[i])
+}
+newHold.join();
+return newHold;
+}
+
+// To enguer and deque means to add to stack
+let inputStack = [];
+let ouputStack = [];
+function enqueue(stackInput, item) {
+return stackInput.push(item)
+}
+function createBase(baseNumber) {
+return function(N) {
+// we are referencing baseNumber here even though it was declared
+// outside of this function. Closures allow us to do this in JavaScript
+return baseNumber + N;
+}
+}
+var addSix = createBase(6);
+
+var arrayOfIntegers = [2, 5, 1, 4, 9, 6, 3, 7];
+var upperBound = 9;
+var lowerBound = 1;
+findMissingNumber(arrayOfIntegers, upperBound, lowerBound); // 8
+function findMissingNumber(arrayOfIntegers, upperBound, lowerBound) {
+// Iterate through array to find the sum of the numbers
+var sumOfIntegers = 0;
+for (var i = 0; i < arrayOfIntegers.length; i++) {
+sumOfIntegers += arrayOfIntegers[i];
+}
+// Find theoretical sum of the consecutive numbers using a variation of Gauss Sum.
+// Formula: [(N * (N + 1)) / 2] - [(M * (M - 1)) / 2];
+// N is the upper bound and M is the lower bound
+upperLimitSum = (upperBound * (upperBound + 1)) / 2;
+lowerLimitSum = (lowerBound * (lowerBound - 1)) / 2;
+theoreticalSum = upperLimitSum - lowerLimitSum;
+return theoreticalSum - sumOfIntegers;
+}
+
+
+// ==================== Module pattern
+
+• Factory function: -- basically create functions main purpose is to create object
+○ Example 
+  § Function createCircle(param) {
+    Return {  radius,
+      draw() {
+       }
+    };
+  }
+○ const myCircle = createCircle(1);
+
+• Constructor functions: function Circle(radius) {
+  this.radius = radius;
+  this.draw = function() {
+    console.log('draw');
+  }
+}
+const circle = new circle(1);
+
+
+// =============== thinking outside the box of React -----
+// Do not be set up on the mindset of React
+// React is a virtual Dom tree which return an object
+  // there might be situation where you have to do stuff outside the react dome tree so you musht think aout the box to do so
