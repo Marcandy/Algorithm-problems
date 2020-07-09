d0c1ace2ebe871d4904283af0ec38250043585bf
Marcandy <marcandy.nj@gmail.com>
2020-07-08 22:57:11 -0400

    2d array problem

---
+++ pratice/2d-array.js
@@ -0,0 +1,11 @@
+/* Version 1: Every row is sorted left to right 
+in non-decreasing order. The first integer of each row is greater 
+or equal to the last integer of the previous row.
+*/
+
+/*
+Version 2: Every row is sorted left to right in non-decreasing 
+order. Every column is sorted top to bottom in non-decreasing order. 
+There are no guarantees that the first item of any row or 
+column relates to the last item in the previous row or column.
+*/
